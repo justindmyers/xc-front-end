6181b47114b6a4d1421c1daab578e412155cf25b
Adam Mornes <Adam.Mornes@xcentium.com>
2018-05-04 09:47:46 -0500

    Hooked /src/sass/style.scss into Demo - Cleaned up Bootstrap specific variables

--- demo/index.js
+++ demo/index.js
@@ -9,6 +9,7 @@ import VueScrollTo from 'vue-scrollto';
 
 // Import production application components
 import '@/index.js';
+import 'src/sass/style.scss';
 
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import './app/sass/styles.scss';
