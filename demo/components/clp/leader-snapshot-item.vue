8cbc94c64ea72939a8f39c2880573495916d06b8
Adam Mornes <Adam.Mornes@xcentium.com>
2018-05-04 14:08:38 -0500

    Added basic Markup and Styles for Leader Snapshot Item component

--- demo/components/clp/leader-snapshot-item.vue
+++ demo/components/clp/leader-snapshot-item.vue
@@ -1,5 +1,11 @@
 <template>
     <div class="c-snapshot-item">
-        
+        <img class="c-snapshot-item__img" src="" />
+        <h3 class="c-snapshot-item__heading">Mr. Michael Podemski</h3>
+        <p class="c-snapshot-item__description">President</p>
+        <a class="c-snapshot-item__mailto" href="mailto:">
+            <span class="fa fa-envelope"></span>
+            Send Email
+        </a>
     </div>
 </template>
--- demo/pages/components/leader-snapshot-item.vue
+++ demo/pages/components/leader-snapshot-item.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>
-        <leader-snapshot-item></leader-snapshot-item>
+    <div class="flex__grid">
+        <div class="flex__cell -12of12 -large-3of12">
+            <leader-snapshot-item></leader-snapshot-item>
+        </div>
     </div>
 </template>
