42f52c3644250eecadfc9eaf734a75aefcadfbd2
Adam Mornes <Adam.Mornes@xcentium.com>
2018-05-07 15:42:36 -0500

    Refactor Leader Snapshot demo component to use updated Grid styles

--- demo/pages/components/leader-snapshot-item.vue
+++ demo/pages/components/leader-snapshot-item.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="flex__grid">
-        <div class="flex__cell -auto -12of12 -large-3of12">
+    <div class="xc-flex__grid">
+        <div class="xc-flex__cell -col-12of12 -col-lg-3of12">
             <leader-snapshot-item></leader-snapshot-item>
         </div>
     </div>
