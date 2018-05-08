389578a63fbd19c440e3c6f842918dfa60a700d3
Adam Mornes <Adam.Mornes@xcentium.com>
2018-05-08 18:03:08 -0500

    Completed Button Link Bar component

--- demo/components/clp/button-link-bar.vue
+++ demo/components/clp/button-link-bar.vue
@@ -1,5 +1,40 @@
 <template>
-    <div class="c-button-links">
-                
+    <div class="c-button-links xc-flex__grid -center ">
+
+        <div class="c-button-links__item xc-flex__cell -col-5of10 -col-sm-2of10 -col-lg-2of10">
+            <img class="c-button-links__icon" src="/assets/images/icons/reports.png" alt="Reports" />
+            <a class="c-button-links__link" href="#">
+                Reports <span class="c-button-links__link-icon fa fa-angle-right"></span>
+            </a>
+        </div>
+
+        <div class="c-button-links__item c-button-links__item--xs-border-none xc-flex__cell -col-5of10 -col-sm-2of10 -col-lg-2of10">
+            <img class="c-button-links__icon" src="/assets/images/icons/upload-cpe.png" alt="Upload CPE" />
+            <a class="c-button-links__link" href="#">
+                Upload CPE <span class="c-button-links__link-icon fa fa-angle-right"></span>
+            </a>
+        </div>
+
+        <div class="c-button-links__item xc-flex__cell -col-5of10 -col-sm-2of10 -col-lg-2of10">
+            <img class="c-button-links__icon" src="/assets/images/icons/renewal-message.png" alt="Renewal Message" />
+            <a class="c-button-links__link" href="#">
+                Renewal Message <span class="c-button-links__link-icon fa fa-angle-right"></span>
+            </a>
+        </div>
+
+        <div class="c-button-links__item c-button-links__item--xs-border-none xc-flex__cell -col-5of10 -col-sm-2of10 -col-lg-2of10">
+            <img class="c-button-links__icon" src="/assets/images/icons/resources.png" alt="Resources" />
+            <a class="c-button-links__link" href="#">
+                Resources <span class="c-button-links__link-icon fa fa-angle-right"></span>
+            </a>
+        </div>
+
+        <div class="c-button-links__item c-button-links__item--border-none c-button-links__item--xs-text-right xc-flex__cell -col-10of10 -col-sm-2of10 -col-lg-2of10">
+            <img class="c-button-links__icon" src="/assets/images/icons/marketing-storefront.png" alt="Marketing Storefront" />
+            <a class="c-button-links__link" href="#">
+                Marketing Storefront <span class="c-button-links__link-icon fa fa-angle-right"></span>
+            </a>
+        </div>
+
     </div>
 </template>
