b44877610dc1bd58e1cbe79e693cea5f052f18c6
Adam Mornes <Adam.Mornes@xcentium.com>
2018-05-09 17:57:07 -0500

    Refactored Button Link Bar styles and added optional text

--- demo/components/clp/button-link-bar.vue
+++ demo/components/clp/button-link-bar.vue
@@ -1,40 +1,43 @@
 <template>
     <div class="c-button-links xc-flex__grid -center ">
 
-        <div class="c-button-links__item xc-flex__cell -col-5of10 -col-sm-2of10 -col-lg-2of10">
+        <a class="c-button-links__item xc-flex__cell -col-5of10 -col-sm-2of10 -col-lg-2of10" href="#">
             <img class="c-button-links__icon" src="/assets/images/icons/reports.png" alt="Reports" />
-            <a class="c-button-links__link" href="#">
-                Reports <span class="c-button-links__link-icon fa fa-angle-right"></span>
-            </a>
-        </div>
+            <div class="c-button-links__heading">
+                Reports <span class="c-button-links__heading-icon fa fa-angle-right"></span>
+            </div>
+            <p class="c-button-links__description">The latest from the industry and ISACA</p>
+        </a>
 
-        <div class="c-button-links__item c-button-links__item--xs-border-none xc-flex__cell -col-5of10 -col-sm-2of10 -col-lg-2of10">
+        <a class="c-button-links__item c-button-links__item--xs-border-none xc-flex__cell -col-5of10 -col-sm-2of10 -col-lg-2of10" href="#">
             <img class="c-button-links__icon" src="/assets/images/icons/upload-cpe.png" alt="Upload CPE" />
-            <a class="c-button-links__link" href="#">
-                Upload CPE <span class="c-button-links__link-icon fa fa-angle-right"></span>
-            </a>
-        </div>
+            <div class="c-button-links__heading">
+                Upload CPE <span class="c-button-links__heading-icon fa fa-angle-right"></span>
+            </div>
+            <p class="c-button-links__description">Up-to date information and expertise on core IT challenges</p>
+        </a>
 
-        <div class="c-button-links__item xc-flex__cell -col-5of10 -col-sm-2of10 -col-lg-2of10">
+        <a class="c-button-links__item xc-flex__cell -col-5of10 -col-sm-2of10 -col-lg-2of10 " href="#">
             <img class="c-button-links__icon" src="/assets/images/icons/renewal-message.png" alt="Renewal Message" />
-            <a class="c-button-links__link" href="#">
-                Renewal Message <span class="c-button-links__link-icon fa fa-angle-right"></span>
-            </a>
-        </div>
+            <div class="c-button-links__heading">
+                Renewal Message <span class="c-button-links__heading-icon fa fa-angle-right"></span>
+            </div>
+            <p class="c-button-links__description">In-depth articles on pressing industry topics</p>
+        </a>
 
-        <div class="c-button-links__item c-button-links__item--xs-border-none xc-flex__cell -col-5of10 -col-sm-2of10 -col-lg-2of10">
+        <a class="c-button-links__item c-button-links__item--xs-border-none xc-flex__cell -col-5of10 -col-sm-2of10 -col-lg-2of10" href="#">
             <img class="c-button-links__icon" src="/assets/images/icons/resources.png" alt="Resources" />
-            <a class="c-button-links__link" href="#">
-                Resources <span class="c-button-links__link-icon fa fa-angle-right"></span>
-            </a>
-        </div>
+            <div class="c-button-links__heading">
+                Resources <span class="c-button-links__heading-icon fa fa-angle-right"></span>
+            </div>
+        </a>
 
-        <div class="c-button-links__item c-button-links__item--border-none c-button-links__item--xs-text-right xc-flex__cell -col-10of10 -col-sm-2of10 -col-lg-2of10">
+        <a class="c-button-links__item c-button-links__item--border-none c-button-links__item--xs-text-right xc-flex__cell -col-10of10 -col-sm-2of10 -col-lg-2of10" href="#">
             <img class="c-button-links__icon" src="/assets/images/icons/marketing-storefront.png" alt="Marketing Storefront" />
-            <a class="c-button-links__link" href="#">
-                Marketing Storefront <span class="c-button-links__link-icon fa fa-angle-right"></span>
-            </a>
-        </div>
+            <div class="c-button-links__heading">
+                Marketing Storefront <span class="c-button-links__heading-icon fa fa-angle-right"></span>
+            </div>
+        </a>
 
     </div>
 </template>
