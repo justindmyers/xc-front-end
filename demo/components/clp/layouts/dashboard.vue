7f81f50e449567ec83e0ead063a7f316ab0215d1
Adam Mornes <Adam.Mornes@xcentium.com>
2018-05-10 11:31:30 -0500

    Added comments to clarify where to find components referenced within CLP Dashboard layout

--- demo/components/clp/layouts/dashboard.vue
+++ demo/components/clp/layouts/dashboard.vue
@@ -2,29 +2,44 @@
     <div class="l-clp-dashboard">
 
         <div class="c-section">
+
+            <!-- ../button-link-bar.vue -->
             <button-link-bar></button-link-bar>
+
         </div>
          
         <div class="c-section">
             <div class="xc-flex__grid">
 
                 <div class="xc-flex__cell -col-12of12 -col-sm-8of12 -sm-equal">
+
+                    <!-- ../updates.vue -->
                     <updates></updates>
+
                 </div>
 
                 <div class="xc-flex__cell -col-12of12 -col-sm-4of12 -sm-equal">
+
+                    <!-- ../document-repository.vue -->
                     <document-repository></document-repository>
+
                 </div>
 
             </div>
         </div>
 
         <div class="c-section c-section--shadow">
+
+            <!-- ../chapter-stat.vue -->
             <chapter-stats></chapter-stats>
+
         </div>
 
         <div class="c-section">
+
+            <!-- ../leader-list.vue -->
             <leader-list></leader-list>
+
         </div>
 
     </div>
