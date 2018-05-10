8c2b1c5b6d893c9f3e7b8413a036192a2bb2fc1f
Adam Mornes <Adam.Mornes@xcentium.com>
2018-05-10 11:27:48 -0500

    Created .c-section style component for section shadows

--- demo/components/clp/layouts/dashboard.vue
+++ demo/components/clp/layouts/dashboard.vue
@@ -1,23 +1,31 @@
 <template>
     <div class="l-clp-dashboard">
 
-        <button-link-bar></button-link-bar>
+        <div class="c-section">
+            <button-link-bar></button-link-bar>
+        </div>
+         
+        <div class="c-section">
+            <div class="xc-flex__grid">
 
-        <div class="xc-flex__grid">
+                <div class="xc-flex__cell -col-12of12 -col-sm-8of12 -sm-equal">
+                    <updates></updates>
+                </div>
 
-            <div class="xc-flex__cell -col-12of12 -col-sm-8of12 -sm-equal">
-                <updates></updates>
+                <div class="xc-flex__cell -col-12of12 -col-sm-4of12 -sm-equal">
+                    <document-repository></document-repository>
+                </div>
+
             </div>
-
-            <div class="xc-flex__cell -col-12of12 -col-sm-4of12 -sm-equal">
-                <document-repository></document-repository>
-            </div>
-
         </div>
 
-        <chapter-stats></chapter-stats>
+        <div class="c-section c-section--shadow">
+            <chapter-stats></chapter-stats>
+        </div>
 
-        <leader-list></leader-list>
+        <div class="c-section">
+            <leader-list></leader-list>
+        </div>
 
     </div>
 </template>
